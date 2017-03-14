df4d56485c6cbe93806b81c2d6b3d13a8ca1affb
Andrea Velasquez <a.velasquez12@uniandes.edu.co>
2017-03-14 11:24:59 -0500

    todos los valores dan nan :(

--- EulerFluid1D.py
+++ EulerFluid1D.py
@@ -60,6 +60,8 @@ def MacCormack():
             U2s[i] = U2[i][n] - dt/dx*(F2[i+1][n] - F2[i][n]) 
             U3s[i] = U3[i][n] - dt/dx*(F3[i+1][n] - F3[i][n]) 
 
+        for i in range(I-1):
+
             F1s[i] = U2s[i]
             F2s[i] = (U2s[i])**2/U1s[i] + (gamma-1)*(U3s[i] - 0.5*(U2s[i])**2/U1s[i])
             F3s[i] = U2s[i]/U1s[i]*(U3s[i] + (gamma-1)*(U3s[i] - 0.5*(U2s[i])**2/U1s[i]))
@@ -70,6 +72,8 @@ def MacCormack():
             U2[i][n+1] = 0.5*(U2[i][n] + U2s[i] - dt/dx*(F2s[i] - F2s[i-1]))
             U3[i][n+1] = 0.5*(U3[i][n] + U3s[i] - dt/dx*(F3s[i] - F3s[i-1]))
             
+        for i in range(I-1):
+
             F1[i][n+1] = U2[i][n+1]
             F2[i][n+1] = (U2[i][n+1])**2/U1[i][n+1] + (gamma-1)*(U3[i][n+1] - 0.5*(U2[i][n+1])**2/U1[i][n+1])
             F3[i][n+1] = U2[i][n+1]/U1[i][n+1]*(U3[i][n+1] + (gamma-1)*(U3[i][n+1] - 0.5*(U2[i][n+1])**2/U1[i][n+1]))
@@ -86,12 +90,15 @@ def MacCormack():
     uf = []
     for i in range(I-1):
         uf.append(U2[i][N-1]/U1[i][N-1])
+    
 
     return rhof, Pf, uf
+
 
    
 rho, P, u = MacCormack()
 
+'''
 plt.plot(x, rho, label='rho')
 plt.legend()
 plt.show()
@@ -103,5 +110,5 @@ plt.show()
 plt.plot(x, u, label='u')
 plt.legend()
 plt.show()
-
+'''
 
