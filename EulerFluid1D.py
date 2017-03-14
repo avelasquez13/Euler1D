b9d9333b21cb5db126e3024c3617c8ca1394f380
Andrea Velasquez <a.velasquez12@uniandes.edu.co>
2017-03-14 11:15:07 -0500

    no tiene errores pero no grafica

--- EulerFluid1D.py
+++ EulerFluid1D.py
@@ -5,7 +5,7 @@ dx = 0.01
 dt = 0.01
 I = int(1/dx)
 N = int(1/dt)
-x = np.linspace(0, 1, I)
+x = np.linspace(0, 1, I-1)
 t = [1.0, 2.0, 3.0]
 gamma = 1.4
 
@@ -27,20 +27,20 @@ def e_0(x):
 def MacCormack():
     U1 = np.zeros([I, N])
     for i in range(I):
-        U1[i][0] = rho_0(i)
+        U1[i][0] = rho_0(i*dx)
 
     U2 = np.zeros([I, N])
 
     U3 = np.zeros([I, N])
     for i in range(I):
-        U3[i][0] = e_0(i)
+        U3[i][0] = e_0(i*dx)
 
 
     F1 = np.zeros([I, N])
 
     F2 = np.zeros([I, N])
     for i in range(I):
-        F2[i][0] = P_0(x)
+        F2[i][0] = P_0(i*dx)
 
     F3 = np.zeros([I, N])
 
@@ -70,14 +70,38 @@ def MacCormack():
             U2[i][n+1] = 0.5*(U2[i][n] + U2s[i] - dt/dx*(F2s[i] - F2s[i-1]))
             U3[i][n+1] = 0.5*(U3[i][n] + U3s[i] - dt/dx*(F3s[i] - F3s[i-1]))
             
-            F1[i][n+1] = U2[i]
-            F2[i][n+1] = (U2[i])**2/U1[i] + (gamma-1)*(U3[i] - 0.5*(U2[i])**2/U1[i])
-            F3[i][n+1] = U2[i]/U1[i]*(U3[i] + (gamma-1)*(U3[i] - 0.5*(U2[i])**2/U1[i]))
+            F1[i][n+1] = U2[i][n+1]
+            F2[i][n+1] = (U2[i][n+1])**2/U1[i][n+1] + (gamma-1)*(U3[i][n+1] - 0.5*(U2[i][n+1])**2/U1[i][n+1])
+            F3[i][n+1] = U2[i][n+1]/U1[i][n+1]*(U3[i][n+1] + (gamma-1)*(U3[i][n+1] - 0.5*(U2[i][n+1])**2/U1[i][n+1]))
 
 
-    return u1[N-1]
+    rhof = []
+    for i in range(I-1):
+        rhof.append(U1[i][N-1])
 
-    
+    Pf = []
+    for i in range(I-1):
+        Pf.append(F2[i][N-1]-(U2[i][N-1])**2/U1[i][N-1])
 
+    uf = []
+    for i in range(I-1):
+        uf.append(U2[i][N-1]/U1[i][N-1])
+
+    return rhof, Pf, uf
+
+   
+rho, P, u = MacCormack()
+
+plt.plot(x, rho, label='rho')
+plt.legend()
+plt.show()
+
+plt.plot(x, P, label='P')
+plt.legend()
+plt.show()
+
+plt.plot(x, u, label='u')
+plt.legend()
+plt.show()
 
 
